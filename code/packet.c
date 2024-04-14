121db10f7c19e33ddfc39486c2d845f8d99daefa
elyazya <113930251+elyazya@users.noreply.github.com>
2024-04-14 10:51:25 -0400

    adding list implementation

--- code/packet.c
+++ code/packet.c
@@ -20,3 +20,30 @@ int get_data_size(tcp_packet *pkt)
     return pkt->hdr.data_size;
 }
 
+// function to add a packet to the list's end
+void push(packet_list** head_ref, tcp_packet* pkt)
+{
+    packet_list* new_node = (packet_list*) malloc(sizeof(packet_list));
+    new_node->pkt = pkt;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+    else {
+        packet_list* temp = *head_ref;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+
+// function to remove the first packet from the list
+void pop(packet_list** head_ref)
+{
+    packet_list* temp = *head_ref;
+    *head_ref = temp->next;
+    free(temp);
+}
